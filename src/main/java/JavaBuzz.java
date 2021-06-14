826962f5c7ceee33d4b8488a8b92e7c445ebf12b
Adam Jonas <adamjonas25@gmail.com>
2021-06-14 12:58:41 +0100

    Define main method in JavaBuzz to print results 1 to 100

--- src/main/java/JavaBuzz.java
+++ src/main/java/JavaBuzz.java
@@ -1,4 +1,10 @@
 public class JavaBuzz {
+    public static void main() {
+        for (int i = 1; i < 101; i++) {
+            System.out.println(javaBuzz(i));
+        }
+    }
+
     public static String javaBuzz(int number) {
         if (number % 15 == 0) { return "JavaBuzz"; }
         if (number % 5 == 0) { return "Buzz"; }
--- src/test/JavaBuzzTest.java
+++ src/test/JavaBuzzTest.java
@@ -1,13 +1,30 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class JavaBuzzTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
 
     @Test
     @DisplayName("Number is returned as string if neither divisible by 3 or 5")
-
     void regularNumbers() {
         assertEquals(JavaBuzz.javaBuzz(2), "2");
     }
@@ -28,5 +45,16 @@ class JavaBuzzTest {
     @DisplayName("JavaBuzz is returned if number is divisible by 15")
     void divisibleBy15() {
         assertEquals(JavaBuzz.javaBuzz(15), "JavaBuzz");
+    }
+
+    @Test
+    @DisplayName("Test output is as expected")
+    void main() {
+        String outputStart = "1\n2\nJava\n4\nBuzz\nJava\n7\n8\nJava\nBuzz\n11\nJava\n13\n14\nJavaBuzz";
+        String outputEnd = "86\nJava\n88\n89\nJavaBuzz\n91\n92\nJava\n94\nBuzz\nJava\n97\n98\nJava\nBuzz";
+
+        JavaBuzz.main();
+        assertTrue(outputStreamCaptor.toString().trim().contains(outputStart));
+        assertTrue(outputStreamCaptor.toString().trim().contains(outputEnd));
     }
 }
